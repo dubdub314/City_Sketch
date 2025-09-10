0679a50220fb695d363fa6d12cd8b311245a487c
MetaCityLab <manner.wei@gmail.com>
2025-09-10 15:13:11 +0800

    Delete citysketch directory

--- citysketch/api_clients.py
+++
@@ -1,8 +0,0 @@
-class GoogleMapsStub:
-    """Placeholder: fill with your real Google Maps API calls if you do not have RealMap shapefiles.
-    Use it only when RealMap is unavailable. """
-    def __init__(self, api_key: str):
-        self.api_key = api_key
-    def geocode(self, name: str):
-        # Return fake coordinates for now
-        return {'name': name, 'lat': 0.0, 'lng': 0.0}
--- citysketch/constants.py
+++
@@ -1,6 +0,0 @@
-TYPE_VALUES = ["Landmark","Node","Path","Edge","District"]
-REL_TYPES = ["Gaze","Connection","Navigation","Fusion","Abruptness","Penetration"]
-EDGE_CAT = {
-    "SSG":0, "REL_Gaze":1, "REL_Connection":2, "REL_Navigation":3, "REL_Fusion":4, "REL_Abruptness":5, "REL_Penetration":6,
-    "TOPO_in_district":7, "TOPO_on_line":8, "TOPO_near_endpoint":9, "TOPO_touches_boundary":10
-}
